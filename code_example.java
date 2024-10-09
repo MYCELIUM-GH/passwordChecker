d7f6210a6dade1c129d0744ef56315d08902a0ff
MYCELIUM-GH <felelem.s@gmail.com>
2024-10-09 13:43:55 +0100

    some fixes + comments

--- code_example.java
+++ code_example.java
@@ -1,87 +1,91 @@
 import java.util.Scanner;
-public class Password {
+public class code_example {
 
 public static void main(String[] args) {
     //INITIAL VALUES. 0 = false, 1 = true
-    int min =8;
-    int max=16;
-    //INT 0 IT'S LIKE BOOLEAN FALSE
-    int digit=0;
-    int special=0;
-    int upCount=0;
-    int loCount=0;
-    String password;
-    Scanner scan = new Scanner(System.in);
-    System.out.println(" Enter Your Password:");
-        password = scan.nextLine();
+    int min=8,max=16,digit=0,special=0,upCount=0,loCount=0; //INT 0 IT'S LIKE BOOLEAN FALSE
+    
+    Scanner scn=new Scanner(System.in); //CREATING NEW VARIABLE. TYPE - SCANNER, NAME - scn
+    
+    System.out.println("Enter Your Password:"); //PROMPT TO ASK USER FOR TYPING
+    String password=scn.nextLine(); //IT WILL SAVE USER INPUT INTO VARIABLE CALLED password
+    
     //STATEMENT TO CHECK IS PASSWORD LENGTH IS MORE THAN 8 AND LESS THAN 16
-    if(password.length()>=min&&password.length()<=max){ //IF TRUE - CONTINUE, IF NOT - JUMP TO "ELSE"
-        for(int i =0;i<password.length();i++){
-            char c = password.charAt(i);
-            if(Character.isUpperCase(c)){
+    if(password.length()>=min&&password.length()<=max) { //CHECKING FOR PASSWORD LENGTH >=8 AND <=16. IF TRUE - CONTINUE, IF NOT - JUMP TO "ELSE"
+        for(int i=0;i<password.length();i++) { //LOOP. CREATES VALUE 0, WILL ADD +1 TO VALUE BEFORE VALUE WILL BE EVEN THAN PASSWORDS LENGTH
+            char c = password.charAt(i); //IT WILL CHECK EVERY CHAR IN PASSWORD (PASSWORD LENGTH) TIMES
+            
+            if(Character.isUpperCase(c)) { //IF SOMEWHERE IN PASSWORD UPPERCASE IS EXISTING - RETURN 1 (TRUE)
                 upCount++;
             }
-            if(Character.isLowerCase(c)){
+            if(Character.isLowerCase(c)) { //IF SOMEWHERE IN PASSWORD LOWERCASE IS EXISTING - RETURN 1 (TRUE)
                 loCount++;
             }
-            if(Character.isDigit(c)){
+            if(Character.isDigit(c)) { //IF SOMEWHERE IN PASSWORD NUMBER IS EXISTING - RETURN 1 (TRUE)
                 digit++;
             }
-            if(c>=33&&c<=46||c==64){
+            if(c>=33&&c<=46||c==64) {
                 special++;
             }
+        } //THE END OF "FOR" LOOP
+
+        if(special>=1&&loCount>=1&&upCount>=1&&digit>=1) { //IF ALL OF CONDITIONS ARE TRUE - PRINT TEXT
+            System.out.println("Password is good.");
         }
-        if(special>=1&&loCount>=1&&upCount>=1&&digit>=1){
-            System.out.println(" Password is good:");
-        }
+    } //THE END OF CONDITIONS CHECKING
 
-    }
-
-    if(password.length()<min){
-
-        for(int i =0;i<password.length();i++){
+    if(password.length()<min) { //IF PASSWORD LENGTH LESS THAN 8 - START LOOP
+        for(int i=0;i<password.length();i++) {
             char c = password.charAt(i);
-            if(Character.isLowerCase(c)){
+            
+            if(Character.isLowerCase(c)) {
                 loCount++;
             }
+        }
+
+        if(loCount>0) {
+            System.out.println("Password must be atleast "+min+" characters!");
+            System.out.println("You need atleast one upper case chracter!");
+            System.out.println("You need atleast one digit!");
+            System.out.println("You need atleast one special chracter!");
+        }
+    } //THE END OF CHECKING FOR MIN LENGTH
+
+    else if(password.length()<min&&upCount>1) {
+        for(int i =0;i<password.length();i++) {
+            char c =password.charAt(i);
+
+            if(Character.isLowerCase(c)) {
+                loCount++;
             }
 
-        if(loCount>0){
-            System.out.println(" Password must be atleat "+min+" characters:");
-            System.out.println(" You need atleast one upper case chracter:");
+            if(Character.isUpperCase(c)) {
+                upCount++;
+            }
+        }
+        
+        if(loCount>0&&upCount>0) {
+            System.out.println(" Password must be atleast "+min+" chracters:");
             System.out.println(" You need atleast one digit:");
             System.out.println(" You need atleast one special chracter:");
-
-
-
-    }
-    }
-    else if(password.length()<min&&upCount>1){
-        for(int i =0;i<password.length();i++){
-        char c =password.charAt(i);
-        if(Character.isLowerCase(c)){
-            loCount++;
         }
-         if(Character.isUpperCase(c)){
-            upCount++;
-        }
-        }
-        if(loCount>0&&upCount>0){
-        System.out.println(" Password must be atleast "+min+" chracters:");
-        System.out.println(" You need atleast one digit:");
-        System.out.println(" You need atleast one special chracter:");
     }
-    }
-     if(password.length()>max||password.length()>=max&&upCount>1&&loCount>1&&digit>1){
-         System.out.println(" Password is too long.Limit is "+max+" chracters:");
-                 System.out.println(" You need atleast one special chracter:");
+
+        if(password.length()>max||password.length()>=max&&upCount>1&&loCount>1&&digit>1) {
+            System.out.println(" Password is too long.Limit is "+max+" chracters:");
+            System.out.println(" You need atleast one special chracter:");
 
         }
-      if(password.length()>=min&&password.length()<=max&&loCount>0&&upCount>0&&digit>0&&special==0){
-         System.out.println(" You need atleast a special chracter");
-     }
-      if(password.length()>=min&&password.length()<=max&&loCount>0&&upCount>0&&digit==0&&special==0){
-         System.out.println(" You need atleast one digit:");
-         System.out.println(" You need atleast one special chracter:");
-     }
-   }
+
+        if(password.length()>=min&&password.length()<=max&&loCount>0&&upCount>0&&digit>0&&special==0) {
+            System.out.println(" You need atleast a special chracter");
+        }
+
+        if(password.length()>=min&&password.length()<=max&&loCount>0&&upCount>0&&digit==0&&special==0) {
+            System.out.println(" You need atleast one digit:");
+            System.out.println(" You need atleast one special chracter:");
+        }
+
+    scn.close();
+}
+}
